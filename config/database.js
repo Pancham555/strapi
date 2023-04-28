bdd31bcdae399e01a8f40ebe2959cb78809eb3f3
Pancham Barman <panchamb63@gmail.com>
2023-04-28 07:47:58 +0530

    Added temp db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
     return {
       defaultConnection: "default",
       connection: {
