b230310813680cbd34da23e66da689a5358c488f
Pancham Barman <panchamb63@gmail.com>
2023-04-28 07:57:31 +0530

    Added temp db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,28 @@
 const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false, // set to true if your PostgreSQL server has a valid SSL/TLS certificate
+      },
+    },
+    options: {
+      ssl: false,
+    },
+  },
+});
+
+/*
+
+const parse = require("pg-connection-string").parse;
 module.exports = ({ env }) => {
   // if (env("NODE_ENV") === "production") {
   //   const config = parse(process.env.DATABASE_URL);
@@ -39,3 +63,6 @@ module.exports = ({ env }) => {
     },
   };
 };
+
+
+*/
